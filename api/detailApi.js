e309486177a6abd739adabf7c576fad2cf1a41f8
parmis <parmis.khorshidi@gmail.com>
2025-05-31 16:22:14 +0330

    fix: edit API

--- api/detailApi.js
+++ api/detailApi.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = import.meta.env.VITE_API_URL;
+export const API_URL ='https://moviesapi.codingfront.dev/api/v1';
 //detail page
 //////////////////////////////////////////
 
